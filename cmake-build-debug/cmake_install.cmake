1e705f6380b8ca16939e75638864883fd6df2ed0
hiroyaiyori <vp054az116@gmail.com>
2020-12-08 21:29:09 +0900

    DEL:cmake debug files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/iyori/Desktop/UT/graduate/eisenstat
+# Install script for directory: /Users/hiroya/Desktop/UT/graduate/eisenstat/EisenstatCG
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/iyori/Desktop/UT/graduate/eisenstat/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/hiroya/Desktop/UT/graduate/eisenstat/EisenstatCG/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
